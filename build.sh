23ede12ebc7b6c7d484b81f5ba011c3f84fa9f76
mYmNeo <thomassong2012@gmail.com>
2025-01-01 06:02:57 +0000

    custom build

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+ 
+BUILD_DIR=$(dirname "$0")/build
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+export GO111MODULE=on
+echo "Setting GO111MODULE to" $GO111MODULE
+
+VERSION=`date -u +%Y%m%d`
+LDFLAGS="-X main.VERSION=$VERSION -s -w -X main.SALT=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+GCFLAGS=""
+
+# AMD64 
+OSES=(linux)
+for os in ${OSES[@]}; do
+    suffix=""
+    if [ "$os" == "windows" ]
+    then
+        suffix=".exe"
+    fi
+    env CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -pgo=auto -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_${os}_amd64${suffix} github.com/xtaci/kcptun/client
+    env CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -pgo=auto -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_${os}_amd64${suffix} github.com/xtaci/kcptun/server
+done
+
+# ARM64
+OSES=(linux darwin)
+for os in ${OSES[@]}; do
+   env CGO_ENABLED=0 GOOS=$os GOARCH=arm64 go build -pgo=auto -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o client_${os}_arm64${suffix} github.com/xtaci/kcptun/client
+   env CGO_ENABLED=0 GOOS=$os GOARCH=arm64 go build -pgo=auto -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o server_${os}_arm64${suffix} github.com/xtaci/kcptun/server
+done
+
+
